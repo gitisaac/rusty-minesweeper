4526d6a818f2580b16e3a6d629fc5ab99cd01ca5
Isaac Gonzalez <isaac.gonzalez@satcube.com>
2023-02-19 20:48:28 +0100

    a visible grid, with correct components for cell and gameboard

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,31 @@
+mod components;
+use crate::components::minesweeper::GameBoard;
+use components::minesweeper::GameBoardProps;
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
-}
+struct Model {}
 
-struct CounterComponent {
-    value: i64,
-}
-
-impl Component for CounterComponent {
-    type Message = Msg;
-    type Properties = ();
+impl Component for Model {
+    type Message = ();
+    type Properties = GameBoardProps;
 
     fn create(_ctx: &Context<Self>) -> Self {
-        CounterComponent { value: 0 }
+        Model {}
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                true  // re-renders the component
-            }
-        }
+        false
     }
 
-    // similar to React's render function
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <div class="container">
-                <p>{ self.value }</p>
-                <button onclick={ctx.link().callback(|_| Msg::AddOne)}>{ "Add one" }</button>
+            <div>
+                <GameBoard rows=10 cols=10 />
             </div>
         }
     }
 }
 
-
 fn main() {
-    yew::start_app::<CounterComponent>();
+    yew::start_app::<Model>();
 }
